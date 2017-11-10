9b16c89b1d786b8ca3f6e075063360e1cf2862c2
zhengchanglin <yh.lvdousha@gmail.com>
2017-11-10 14:37:11 +0800

    初始化

---
+++ src/main/java/com/lvdousha/jdbc/hibernate/App.java
@@ -0,0 +1,45 @@
+package com.lvdousha.jdbc.hibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.hibernate.tool.hbm2ddl.SchemaUpdate;
+
+public class App 
+{
+    public static void main( String[] args ){
+    	
+        
+        
+        
+        
+        
+    }
+    
+    public SessionFactory createSessionFactory(){
+    	Configuration cfr = new Configuration().configure();
+    	StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder().configure();  
+    	ServiceRegistry serviceRegistry = serviceRegistryBuilder.build();  
+//    	Metadata metadata = new MetadataSources( serviceRegistry ).buildMetadata();
+//    	metadata.getSessionFactoryBuilder().build();
+    	SessionFactory sf = cfr.buildSessionFactory(serviceRegistry);
+    	return sf;
+    }
+    
+    public void createTableInDatabase(){
+    	Configuration cfr = new Configuration().configure();
+    	String createType = cfr.getProperty("hibernate.hbm2ddl.auto");
+    	if(createType == null || createType.equals("")){
+    		createType = "create";
+    	}
+    	if(createType.equals("create")){
+    		SchemaExport export = new SchemaExport(cfr);  
+            export.create(true, true);
+    	}else if(createType.equals("update")){
+    		SchemaUpdate update = new SchemaUpdate(cfr);
+            update.execute(true, true);
+    	}
+    }
+}
